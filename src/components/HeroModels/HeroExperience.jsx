63559e9db081c5f7bc052661050e24500a090678
Wilson Chan <wilsonchan54@gmail.com>
2025-05-01 17:17:58 -0400

    Added placeholder glb model / converted glb model into react component with npx gltfjsx / rendered model in HeroExperience component

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,6 +1,7 @@
 import { useMediaQuery } from 'react-responsive'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { Robocop } from './Robocop_animated'
 
 const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: '(max-wdith: 1024px)'})
@@ -9,7 +10,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{position:[0,0,15], fov:45} }>
       <ambientLight intensity={0.2} color='red'/>
-      <directionalLight position={[5,5,5]} intensity={1}/>
+      <directionalLight position={[5,5,5]} intensity={3}/>
 
       <OrbitControls      // no props = unrestricted controls
         enablePan={false}
@@ -19,11 +20,17 @@ const HeroExperience = () => {
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
-      
-      <mesh>
-        <boxGeometry args={[1,1,1]}/> 
-        <meshStandardMaterial color='teal' />
-      </mesh>
+
+      <group
+        scale={isMobile? 0.8 : 1.5}
+        position={[0, -1.3, 0]}
+      >
+        <mesh>
+          <boxGeometry args={[1,1,1]}/> 
+          <meshStandardMaterial color='teal' />
+        </mesh>
+        <Robocop />
+      </group>      
     </Canvas>
   )
 }
