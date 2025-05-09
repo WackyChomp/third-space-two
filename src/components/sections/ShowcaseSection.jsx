34c5073169ee2a29fe1cc69bb91d1f9344fa08ae
Wilson Chan <wilsonchan54@gmail.com>
2025-05-09 18:49:32 -0400

    Outlined Showcase section / added styling to left and right of Showcase section in index.css

--- src/components/sections/ShowcaseSection.jsx
+++ src/components/sections/ShowcaseSection.jsx
@@ -4,24 +4,47 @@ const ShowcaseSection = () => {
   const imgOne = `https://media.tenor.com/ZFoOuUSqPBEAAAAi/stalzone-meme.gif`
 
   return (
-    <div id='work'>
+    <div id='work' className='showcase_work'>
       <div className="w-full">
-        <div>
+        <div className='showcase_layout'>
         {/* Left side */}
-        <div className="">
-          <div className="">
-            <img src={imgOne} alt="left-img-here" />
+        <div className="showcase_wrapper_one">
+          <div className="showcase_image_wrapper_one">
+            <img src={imgOne} alt="left-img-here" 
+              className=''
+            />
           </div>
-          <div className="">
+          <div className="showcase_text_content">
             <h2>This is what you came for!</h2>
-            <p>Why else would you be here? There are so many other places where you could be and dedicate your time towards but you're here</p>
+            <p className='text-blue-400 md:text-xl'>
+              Why else would you be here? There are so many other places where you 
+              could be and dedicate your time towards but you're here
+            </p>
           </div>
         </div>
 
         {/* Right side */}
-        <div className="">
-          <div className="">
-            <img src="" alt="" />
+        <div className="showcase_list_wrapper overflow-hidden">
+          <div className="project">
+            <div className="showcase_list_image_wrapper">
+              <img src={imgOne} alt="project list item # 1" 
+            />
+            </div>
+            <h2>Project #1</h2>
+          </div>
+          <div className="project">
+            <div className="showcase_list_image_wrapper">
+              <img src={imgOne} alt="project list item # 1" 
+            />
+            </div>
+            <h2>Project #2</h2>
+          </div>
+          <div className="project">
+            <div className="showcase_list_image_wrapper">
+              <img src={imgOne} alt="project list item # 1" 
+            />
+            </div>
+            <h2>Project #3</h2>
           </div>
         </div>
         </div>
